704a5a82409c52916a6ea011ef6632456e3e49b2
92uros92 <122689123+92uros92@users.noreply.github.com>
2025-06-26 20:29:29 +0200

    --

--- Source/WaveWar/Public/CharacterBase.h
+++ Source/WaveWar/Public/CharacterBase.h
@@ -4,24 +4,80 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "CharacterBase.generated.h"
 
 
 
 
-UCLASS()
+UCLASS(config = Game)
 class WAVEWAR_API ACharacterBase : public ACharacter
 {
 	GENERATED_BODY()
 
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputMappingContext* DefaultMappingContext;
+
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* JumpAction;
+
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* MoveAction;
+
+	/** Look Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* LookAction;
+
 public:
+
 	ACharacterBase();
 
 	virtual void Tick(float DeltaTime) override;
 
+	// APawn interface
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
+
 	virtual void BeginPlay() override;
+
+	/** Called for movement input */
+	void Move(const FInputActionValue& Value);
+
+	/** Called for looking input */
+	void Look(const FInputActionValue& Value);
+
+	AActor* FindPlayerStart() const;
+
+public:
+
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+	void RespawnAtPlayerStart();
+
+	void RespawnAtCheckpoint();
+
+	void SetCheckpointLocation(FVector NewCheckpointLocation);
+
+	void SafePlayerLocation();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector CheckpointLocation;
+
+	FVector SafeLocation;
 
 };
