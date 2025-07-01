4f1d3a1daf533a1d4412d7e7e328721dd593e2d5
92uros92 <122689123+92uros92@users.noreply.github.com>
2025-07-01 19:47:14 +0200

    - In editor: In ABP_Shadow added MainStates (OnGround->InAir->Land) and added GroundLocomotionsState (Idle->Run) - Added ShadowPlayerState() class

---
+++ Source/WaveWar/Private/Player/ShadowPlayerState.cpp
@@ -0,0 +1,13 @@
+// Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+
+
+#include "Player/ShadowPlayerState.h"
+
+
+
+
+
+AShadowPlayerState::AShadowPlayerState()
+{
+	NetUpdateFrequency = 100.0f;
+}
---
+++ Source/WaveWar/Public/Player/ShadowPlayerState.h
@@ -0,0 +1,22 @@
+// Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "ShadowPlayerState.generated.h"
+
+
+
+
+
+UCLASS()
+class WAVEWAR_API AShadowPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+public:
+
+	AShadowPlayerState();
+
+};
