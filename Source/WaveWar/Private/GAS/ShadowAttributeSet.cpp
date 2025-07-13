018a620395706c6468d7b5c46c56511bf44899c6
92uros92 <122689123+92uros92@users.noreply.github.com>
2025-07-13 19:57:10 +0200

    - In ShadowAttributeSet() class added UShadowAttributeSet() constructor, GetLifetimeReplicatedProps(), OnRep_Health() and OnRep_MaxHealth() functions

--- Source/WaveWar/Private/GAS/ShadowAttributeSet.cpp
+++ Source/WaveWar/Private/GAS/ShadowAttributeSet.cpp
@@ -2,4 +2,32 @@
 
 
 #include "GAS/ShadowAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+
+
+
+
+UShadowAttributeSet::UShadowAttributeSet()
+{
+
+}
+
+void UShadowAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UShadowAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UShadowAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+}
+
+void UShadowAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UShadowAttributeSet, Health, OldHealth);
+}
+
+void UShadowAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UShadowAttributeSet, MaxHealth, OldMaxHealth);
+}
--- Source/WaveWar/Public/GAS/ShadowAttributeSet.h
+++ Source/WaveWar/Public/GAS/ShadowAttributeSet.h
@@ -13,4 +13,24 @@ class WAVEWAR_API UShadowAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Health")
+	FGameplayAttributeData Health;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Health")
+	FGameplayAttributeData MaxHealth;
+
+	////****	FUNCTIONS	****////
+
+	UShadowAttributeSet();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
 };
