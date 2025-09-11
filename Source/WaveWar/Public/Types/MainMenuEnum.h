808d8542c817f385189f1a0bb823342c39bc1c67
92uros92 <122689123+92uros92@users.noreply.github.com>
2025-09-11 21:57:46 +0200

    -Added ConfirmScreenWidget() class.

---
+++ Source/WaveWar/Public/Types/MainMenuEnum.h
@@ -0,0 +1,21 @@
+// Uroš Pörš All Rights Reserved
+
+#pragma once
+
+UENUM(BlueprintType)
+enum class EConfirmScreenType : uint8
+{
+	Ok,
+	YesNo,
+	OKCancel,
+	Unknown UMETA(Hidden)
+};
+
+UENUM(BlueprintType)
+enum class EConfirmScreenButtonType : uint8
+{
+	Confirmed,
+	Cancelled,
+	Closed,
+	Unknown UMETA(Hidden)
+};
--- Source/WaveWar/WaveWar.Build.cs
+++ Source/WaveWar/WaveWar.Build.cs
@@ -8,7 +8,7 @@ public class WaveWar : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", "UMG", "CommonInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayTags", "GameplayTasks", "Niagara", "AIModule" });
 
